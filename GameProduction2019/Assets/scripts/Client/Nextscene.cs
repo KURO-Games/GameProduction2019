7b94e02fcd96c08eb3dd6a1942ae546b19d99fb2
KURO-Games <kuro@kuro.games>
2019-06-01 16:28:50 +0900

    commit2

---
+++ GameProduction2019/Assets/scripts/Client/Nextscene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Nextscene : MonoBehaviour
+{
+    Scene scene;
+    public void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+    }
+    public void ButtonPush()
+    {
+
+        if (SceneManager.sceneCountInBuildSettings-1 == scene.buildIndex)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            SceneManager.LoadScene(scene.buildIndex + 1);
+        }
+    }
+}
